eb438fc60fe0b90e0b54645168c1a4b193f5097b
KhathutsheloCarter <121690232+KhathutsheloCarter@users.noreply.github.com>
2023-09-11 15:07:11 +0200

    Add files via upload

---
+++ 4. data types/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.data_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "John";
+            int age = 18;
+            bool isTall = true;
+            double weight = 80.2;
+
+            Console.WriteLine("His name is " + name + " he is " + age + " it is " + isTall + " that he is tall");
+            Console.WriteLine("his weiht is " + weight);
+
+            name = " khathutshelo";
+            age = 23;
+            isTall = true;
+            weight = 90;
+
+            Console.WriteLine("His name is " + name +"\n" + " he is " + age +"\n " + " it is " + isTall + " that he is tall");
+            Console.WriteLine("his weiht is " + weight);
+
+            Console.ReadLine();
+
+        }
+    }
+}
